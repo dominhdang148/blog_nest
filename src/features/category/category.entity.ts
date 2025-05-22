267669815ed06ceaa39e514cc8d72bc17968f778
Đỗ Minh Đăng <myclone235@gmail.com>
2025-05-22 21:07:46 +0700

    #1 - Create Category entity successfully, table initialized ✅ (#8)

---
+++ src/features/category/category.entity.ts
@@ -0,0 +1,33 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('tbl_category')
+export class Category {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+  @Column({
+    length: 50,
+    unique: true,
+    nullable: false,
+  })
+  name: string;
+
+  @Column({
+    length: 50,
+    unique: true,
+    nullable: false,
+    name: 'url_slug',
+  })
+  urlSlug: string;
+
+  @Column({
+    length: 500,
+  })
+  description: string;
+
+  @Column({
+    default: false,
+    nullable: false,
+    name: 'show_on_menu',
+  })
+  showOnMenu: boolean;
+}
