ac5d63ebb07b1e718bf539f1d6d168d70ec73a93
Đỗ Minh Đăng <myclone235@gmail.com>
2025-05-23 23:55:42 +0700

    #4 Post entity created successfully (#13)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { CategoryModule } from './features/category/category.module';
 import { Category } from './features/category/category.entity';
 import { AuthorModule } from './features/author/author.module';
 import { Author } from './features/author/author.entity';
+import { Post } from './features/post/post.entity';
+import { PostModule } from './features/post/post.module';
 import { TagModule } from './features/tag/tag.module';
 import { Tag } from './features/tag/tag.entity';
 
@@ -24,12 +26,13 @@ import { Tag } from './features/tag/tag.entity';
         username: config.get('DB_USERNAME'),
         password: config.get('DB_PASSWORD'),
         database: config.get('DB_NAME'),
-        entities: [Category, Author, Tag],
+        entities: [Category, Author, Tag, Post],
         synchronize: true,
       }),
     }),
     CategoryModule,
     AuthorModule,
+    PostModule,
     TagModule,
   ],
   providers: [DatabaseService],
