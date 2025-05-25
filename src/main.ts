3b7dd08963040b1af1aa88a6e62f6104b37e91a6
Đỗ Minh Đăng <myclone235@gmail.com>
2025-05-25 11:05:24 +0700

    Release to Main (#20)
    * Removed all unused components (#7)
    
    * Removed all unused services
    
    * #6 - Removed all unused lines/services/controller
    
    * #1 - Create Category entity successfully, table initialized ✅ (#8)
    
    * #9 - fixed. Hot reload work normally (#10)
    
    * #2 - create Author entity successfully (#11)
    
    Issue #2 has been resolved! Confirmed by reviewer
    
    * #3 Tag entity added successfully (#12)
    
    * #4 Post entity created successfully (#13)
    
    * #14 Changed all entities's name (#15)
    
    Pull request for issue #14 has been reviewed and approved
    
    * #5 - relationship established (#16)
    
    Approved!!!!
    
    * #18 - Swagger configuration completed (#19)

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Blog API')
+    .setDescription('This is API document for Blog app')
+    .setVersion('0.1')
+    .addTag('BLOG')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
   if (module.hot) {
     module.hot.accept();
