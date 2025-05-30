6780609a9479a8ee4ccd32006a9ae699e45963b8
dominhdang148 <myclone235@gmail.com>
2025-05-30 20:52:23 +0700

    #28 - added  author endpoints successfully

--- src/features/author/author.module.ts
+++ src/features/author/author.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthorEntity } from './author.entity';
+import { AuthorController } from './controllers/author.controller';
+import { AuthorAdminController } from './controllers/author-admin.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([AuthorEntity])],
+  controllers: [AuthorController, AuthorAdminController],
 })
 export class AuthorModule {}
